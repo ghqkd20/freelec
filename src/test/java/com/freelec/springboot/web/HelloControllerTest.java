f7ef56b4af2bf89cd7d18faed0c6d7576d00d515
ghqkd20 <gjwjdgnsrnlg@naver.com>
2022-02-14 20:20:28 +0900

    테스트 코드 1/2

--- src/test/java/com/freelec/springboot/web/HelloControllerTest.java
+++ src/test/java/com/freelec/springboot/web/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.freelec.springboot.web;
 
+import com.freelec.springboot.web.dto.HelloResponseDto;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +8,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = {HelloController.class})
 class HelloControllerTest {
 
     @Autowired
@@ -25,4 +26,20 @@ class HelloControllerTest {
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
 
     }
+
+    @Test
+    void hello_dto() throws Exception {
+        String name = "hhh";
+        int amount = 1000;
+
+
+        mvc.perform(get("/hello/dto")
+                .param("name",name)
+                .param("amount",String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
+
+    }
+
 }
